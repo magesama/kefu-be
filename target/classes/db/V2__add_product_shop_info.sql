6e62e24dfe7c48ab6fd86938974f894dd603b288
zengbo <zengbo1@ysstech.com>
2025-03-13 17:52:23 +0800

    项目初始化

--- target/classes/db/V2__add_product_shop_info.sql
+++
@@ -1,5 +0,0 @@
--- 添加产品和店铺相关字段
-ALTER TABLE `user_document`
-    ADD COLUMN `product_name` VARCHAR(255) DEFAULT NULL COMMENT '产品名称' AFTER `product_id`,
-    ADD COLUMN `shop_id` BIGINT(20) DEFAULT NULL COMMENT '店铺ID' AFTER `product_name`,
-    ADD COLUMN `shop_name` VARCHAR(255) DEFAULT NULL COMMENT '店铺名称' AFTER `shop_id`; 
